2fe05c87a5007fa4d4e0d877b59d0199e54c39da
jello_chen <jello_chen@live.com>
2016-03-07 01:33:47 +0800

    add the EasyORM Samples

---
+++ src/EasyORM/EasyORM.Samples/Models/T_Department.cs
@@ -0,0 +1,16 @@
+﻿using EasyORM.DbUtils.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyORM.Samples.Models
+{
+    class T_Department
+    {
+        [DataSource(DataSource = DbUtils.ColumnType.AutoIncreament)]
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/EasyORM/EasyORM.Samples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyORM.Samples.Models;
+
+namespace EasyORM.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dc = new DataContext("SQLServer");
+            var query = from q in dc.Set<T_Department>()
+                        select q;
+            foreach (var item in query)
+            {
+                Console.WriteLine("ID:{0},Name:{1}", item.ID, item.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
